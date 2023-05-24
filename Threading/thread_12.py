3e548a27082da227168914fa1a947cc039e9f622
murarikumarjha <jhamk2005@gmail.com>
2023-05-24 12:37:19 +0530

    git commit

---
+++ Threading/thread_12.py
@@ -0,0 +1,17 @@
+# change name of thread by setName 
+# change the name of thread from Thread-1 to 'shantanu'
+
+from threading import Thread
+def display():
+    print('Hello world')
+
+def show():
+    for i in range(3):
+        print('welcome')
+    
+t1 = Thread(target = display)
+t2 = Thread(target = show)
+t2.setName("Aakash")
+t1.name = "shantanu"
+print(t2.getName())
+print(t1.name)
